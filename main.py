f5a0c0e473bcdfa387207a3506375f6a2ddc8b70
Предраг Николић <idmpepe@gmail.com>
2023-02-02 22:26:30 +0100

    perf optimization

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from typing import List, Optional
 import sublime_plugin
 import sublime
 
-string_scope = 'string.quoted.single, string.quoted.double, source.python meta.function-call.arguments.python'
-preceding_text = ['class', 'className']
+string_scope = 'string.quoted.single, string.quoted.double, meta.function-call.arguments'
+# preceding_text = ['class', 'className']
+preceding_text = []
 
 def plugin_loaded():
     views = sublime.active_window().views()
@@ -18,16 +19,28 @@ def plugin_unloaded():
 
 
 class InlineFoldListener(sublime_plugin.ViewEventListener):
+    def __init__(self, view: sublime.View) -> None:
+        super().__init__(view)
+        self.last_cursors = []
+
     def on_load(self) -> None:
         self.view.run_command('inline_fold_all')
 
     def on_selection_modified(self) -> None:
-        # validation
-        r = first_selection_region(self.view)
-        if r is None:
-            return
+        self.schedule()
 
-        cursors: List[sublime.Region] = [r for r in self.view.sel()]
+    def schedule(self):
+        cursors = self.get_cursors()
+        if not cursors:
+            return
+        if self.last_cursors != cursors:
+            self.last_cursors = cursors
+            sublime.set_timeout(lambda: self.run_when_stable(cursors), 50)
+
+    def run_when_stable(self, cursors_to_compare: List[sublime.Region]):
+        if self.last_cursors != cursors_to_compare:
+            return
+        cursors = cursors_to_compare
         strings = find_strings(self.view)
         for string in strings:
             fold_region = get_fold_region(string)
@@ -44,15 +57,21 @@ class InlineFoldListener(sublime_plugin.ViewEventListener):
             if not is_cursor_inside:
                 fold(self.view, fold_region)
 
+    def get_cursors(self) -> List[sublime.Region]:
+        return [r for r in self.view.sel()]
 
 def get_fold_region(string_region: sublime.Region) -> sublime.Region:
     return sublime.Region(string_region.begin() + 1, string_region.end() - 1)
 
 
 def fold(view: sublime.View, fold_r: sublime.Region) -> None:
-    word = view.substr(view.word(view.find_by_class(fold_r.begin(), False, sublime.PointClassification.WORD_START)))
-    if not view.is_folded(fold_r) and word in preceding_text:
-        view.fold(fold_r)
+    if view.is_folded(fold_r):
+        return
+    if preceding_text:
+        word = view.substr(view.word(view.find_by_class(fold_r.begin(), False, sublime.PointClassification.WORD_START)))
+        if word not in preceding_text:
+            return
+    view.fold(fold_r)
 
 
 class InlineFoldAll(sublime_plugin.TextCommand):
