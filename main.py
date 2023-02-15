c2a980fa332272416d99c6d1e6eb5515bac50b7d
Предраг Николић <idmpepe@gmail.com>
2023-02-15 21:59:14 +0100

    move code around

--- main.py
+++ main.py
@@ -48,7 +48,10 @@ class InlineFoldListener(sublime_plugin.ViewEventListener):
         cursors = self.last_cursors
         rules = self.view.settings().get("inline_fold.rules", fallback_rules)
         for rule in rules:
-            fold_regions = get_fold_regions(self.view, rule.get('fold_selector'))
+            fold_regions = self.view.find_by_selector(rule.get('fold_selector'))
+            look_region = get_look_region(self.view)
+            # reduce the number of regions to improve performance in large files
+            fold_regions = list(filter(lambda fold_region: look_region.contains(fold_region), fold_regions))
             for fold_region in fold_regions:
                 line = self.view.line(fold_region)
                 if fold_region.begin() > line.end():
@@ -108,13 +111,7 @@ def first_selection_region(view: sublime.View) -> Optional[sublime.Region]:
     except IndexError:
         return None
 
-def get_fold_regions(view: sublime.View, selector: str) -> List[sublime.Region]:
-    # region Define Filter Region
+def get_look_region(view: sublime.View) -> sublime.Region:
     visible_region = view.visible_region()
     offset =  10000
-    filter_region = sublime.Region(visible_region.begin() - offset, visible_region.end() + offset)
-    # endregion
-    fold_regions = view.find_by_selector(selector)
-    # filter out regions to improve performance in large files
-    fold_regions = list(filter(lambda fold_region: filter_region.contains(fold_region), fold_regions))
-    return fold_regions
+    return sublime.Region(visible_region.begin() - offset, visible_region.end() + offset)
